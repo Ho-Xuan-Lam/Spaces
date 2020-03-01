2480715a10dd4eb555bbbe658d42b9ba63ef6126
Ho-Xuan-Lam <hxlamqtri@gmail.com>
2020-03-01 20:23:46 +0700

    Add project

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('.nav-button').click(function() {
+        $('.nav-button').toggleClass('change');
+    });
+
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("navbar").style.top = "0";
+            $('.nav-menu').addClass('custom-navbar');
+        } else {
+            document.getElementById("navbar").style.top = "-100px";
+            $('.nav-menu').removeClass('custom-navbar');
+        }
+        prevScrollpos = currentScrollPos;
+    }
+});
